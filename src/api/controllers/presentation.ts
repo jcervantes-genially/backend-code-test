805d4fc456191a2d9f54e1eaa1244efe7f268c05
juan cervantes <juancervantes@genially.com>
2022-02-10 12:24:25 +0100

    evitar que las presentaciones se guarden cuando no cumplen las condiciones de titulo y descripcion

--- src/api/controllers/presentation.ts
+++ src/api/controllers/presentation.ts
@@ -1,42 +1,49 @@
 import { Response, Request } from "express";
 
+
+let info: String
+
 function checkTitle (title:String){
-  if (title===undefined || title.length===0){
-    title="Untitle document";
+  if (title===undefined || title.length===0 || title.length<3 || title.length>20){
+    info="Title must be higher than 3 and minor than 20";
+    return 0;
   }else{
-    title=title.substring(0,20);
+    return 1;
   }
-  if (title.length<3 && title.length>0){title='title: '+title}
-  return title;
 }
 
 function checkDescription(description: String){
+  info="description must be minor than 125";
   if (description.length>125){
-    description = description.substring(0,125);
+    return 0;
+  }else{
+    return 1;
   }
-  return description
 }
 
 function createP (req: Request, res: Response) {
   const Presentation = require('../models/presentation');
   const Analytics = require("./analytics");
-
-  let description = checkDescription(req.body.description)
-  let title = checkTitle(req.body.title)
-
-  
-  Presentation.create({
-    title:title,
-    description:description,
-    deleted:false,
-    createdAt:new Date()
-  }).then((doc: any)=>{
-    Analytics.updateAnalytics("presentation")
-    res.json(doc)
-  }).catch((err: any)=>{
-    console.log(err);
-    res.json(err);
-  });
+  if ((checkTitle(req.body.title)===1)){
+    if (checkDescription(req.body.description) ===1){
+      Presentation.create({
+        title:req.body.title,
+        description:req.body.description,
+        deleted:false,
+        createdAt:new Date()
+      }).then((doc: any)=>{
+        Analytics.updateAnalytics("presentation")
+        res.json(doc)
+      }).catch((err: any)=>{
+        console.log(err);
+        res.json(err);
+      });
+    }else{
+      res.status(400).send({ message: info })
+    }
+  }else{
+    res.status(400).send({ message: info })
+  }
 };
 
 function updateP (req: Request, res: Response) {
